d47a0ee99b14ce4e17599fd9c325794f004aa704
Joshua Zhang <jzhang3346>
2023-07-13 16:19:10 -0400

    Co-authored-by: jesshan700 <jesshan700@users.noreply.github.com> Co-authored-by: hellok0 <hellok0@users.noreply.github.com> Co-authored-by: Rayan Khan <RayanBTW@users.noreply.github.com>

---
+++ activities/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC-3dOY7M9ENGueg-qY8KJ9gK4fT-SZFvI",
+  authDomain: "swift-activities.firebaseapp.com",
+  projectId: "swift-activities",
+  storageBucket: "swift-activities.appspot.com",
+  messagingSenderId: "811460040891",
+  appId: "1:811460040891:web:3a76bb495cda1d9b3c719e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
