c478d695939289fd6c153fd2a53ad676770fdfd0
Joshua Zhang <jzhang3346>
2023-07-13 22:48:32 -0400

    Add firebase functionality

--- activities/src/App.js
+++ activities/src/App.js
@@ -1,42 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import "./App.css";
 import logo from "./SWIFT_2021_logo.png";
 import avatar from "./avatar.png";
 import Calendar from "./BasicDateCalendar"
 import AddButton from "./AddButton"
 import ActivitiesList from "./ActivitiesList"
-
-const activities = [
-  {
-    "ActivityName": "Basketball",
-    "Location": "VTC",
-    "Date": "2023-07-14",
-    "Time": "11:00 AM",
-    "Description": "Join us for a friendly basketball game at the VTC court! Let's shoot some hoops and have a great time together.",
-    "Link": "https://dummylink.com",
-    "ContactInfo": "dummyemail@example.com"
-  },
-  {
-    "ActivityName": "Yoga",
-    "Location": "OPC",
-    "Date": "2023-07-15",
-    "Time": "9:30 AM",
-    "Description": "Unwind and rejuvenate with a morning yoga session at the OPC park. Stretch, breathe, and find your inner peace!",
-    "Link": "https://dummylink.com",
-    "ContactInfo": "dummyemail@example.com"
-  },
-  {
-    "ActivityName": "Tennis",
-    "Location": "TYSONS",
-    "Date": "2023-07-18",
-    "Time": "2:00 PM",
-    "Description": "Calling all tennis enthusiasts! Come and play some friendly matches at the Tysons tennis courts. It's game, set, match!",
-    "Link": "https://dummylink.com",
-    "ContactInfo": "dummyemail@example.com"
-  }
-];
+import { collection, addDoc, getDocs } from "firebase/firestore";
+import db from './firebase';
 
 const App = () => {
+  const [activities, setActivites] = useState([]);
+
+  async function addActivities(activity) {
+    try {
+      const docRef = await addDoc(collection(db, "activities"), activity);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  }
+  
+  async function fetchActivities(){
+    await getDocs(collection(db, "activities")).then((querySnapshot)=>{               
+          const newData = querySnapshot.docs.map((doc) => ({...doc.data(), id:doc.id }));
+          setActivites(newData);                
+    });
+  }
+
+  useEffect(()=>{fetchActivities();}, [])
+
   return (
     <div className="app">
       <header className="header">
