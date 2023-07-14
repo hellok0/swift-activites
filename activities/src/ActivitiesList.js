b8dff6b58edec3571aa56984b9dbd4c1b749e9c2
Sourinthone Phonemany <sourinthonephonemany@sourinthonesmbp.lan>
2023-07-13 22:06:30 -0400

    added data and displayed in left panel

---
+++ activities/src/ActivitiesList.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Typography, List, ListItem, ListItemText } from '@mui/material';
+
+const ActivitiesList = ({ activities }) => {
+  return (
+    <div>
+      <Typography variant="h5">Upcoming Events</Typography>
+      <List>
+        {activities.map((activity, index) => (
+          <ListItem key={index} alignItems="flex-start">
+            <ListItemText
+              primary={activity.ActivityName}
+              secondary={
+                <>
+                  <Typography component="span" variant="body2" color="textPrimary">
+                    Location: {activity.Location}
+                  </Typography>
+                  <br />
+                  <Typography component="span" variant="body2" color="textPrimary">
+                    Date: {activity.Date}
+                  </Typography>
+                  <br />
+                  <Typography component="span" variant="body2" color="textPrimary">
+                    Time: {activity.Time}
+                  </Typography>
+                  <br />
+                  <Typography component="span" variant="body2" color="textPrimary">
+                    Description: {activity.Description}
+                  </Typography>
+                  <br />
+                  <Typography component="span" variant="body2" color="textPrimary">
+                    Link: {activity.Link}
+                  </Typography>
+                  <br />
+                  <Typography component="span" variant="body2" color="textPrimary">
+                    Contact Info: {activity.ContactInfo}
+                  </Typography>
+                </>
+              }
+            />
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+};
+
+export default ActivitiesList;
